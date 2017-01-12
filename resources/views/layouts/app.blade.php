1d5d0599d5b0a1aefb4f83eaea685ab28db78522
Matthew Thompson <matthew.thompson.a@gmail.com>
2017-01-12 16:03:00 -0500

    Testing Auth Middleware with about page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
